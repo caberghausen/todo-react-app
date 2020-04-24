7213a9485bf7b9df5667690bcf5918edf96c0266
caberghausen <caberghausen@users.noreply.github.com>
2020-04-23 23:59:27 -0400

    Updates

---
+++ precache-manifest.afcbc1d6dff74ecca1b072e73b498ce7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8817144aafd111d3165c",
+    "url": "/09-todo-react-caberghausen/static/css/main.4179e47c.chunk.css"
+  },
+  {
+    "revision": "8817144aafd111d3165c",
+    "url": "/09-todo-react-caberghausen/static/js/main.8817144a.chunk.js"
+  },
+  {
+    "revision": "787d345cef3a0d1cc32c",
+    "url": "/09-todo-react-caberghausen/static/js/1.787d345c.chunk.js"
+  },
+  {
+    "revision": "ff5f2a83334cbac23963",
+    "url": "/09-todo-react-caberghausen/static/js/runtime~main.ff5f2a83.js"
+  },
+  {
+    "revision": "4a268c48518be40d8f907366c1d5e96d",
+    "url": "/09-todo-react-caberghausen/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/09-todo-react-caberghausen/precache-manifest.6167b8375299c9877bb45cac8e7e7f05.js"
+  "/09-todo-react-caberghausen/precache-manifest.afcbc1d6dff74ecca1b072e73b498ce7.js"
 );
 
 workbox.clientsClaim();
